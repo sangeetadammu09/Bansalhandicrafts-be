83085800b7b50f6ec2ef782e904316480c7873f9
sangeetadammo09 <sangeetadammu12@gmail.com>
2024-10-18 17:11:30 +0530

    aws deployment

--- config/db.js
+++ config/db.js
@@ -2,22 +2,23 @@ const config = require ("config");
 const mongoose = require ("mongoose");
 //console.log(config.get('url'));
 const connectDB =async()=>{
-    // try{
-    //     await mongoose.connect(config.get('url'),{useNewUrlParser:true, useUnifiedTopology:true, useFindAndModify: false},()=>{
-    //         console.log('Database Connected Successfully');
-            
-    //     })
-
-    // }catch(err){
-    //           console.log(err.message);
-    // }
     try{
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING,{useNewUrlParser:true, useUnifiedTopology:true, useFindAndModify: false});
-        console.log('DB connected successfully', connect.connection.host, connect.connection.name)
-     }catch(err){
-         console.error(err);
-         process.exit(1);
-     }
+        const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
+        const connectionString = `mongodb+srv://sangeetadammu12:${password}@dev-cluster.wftp2.mongodb.net/?retryWrites=true&w=majority&appName=dev-cluster`; // clustore url
+       const connect = await mongoose.connect(connectionString,{useNewUrlParser: true,useUnifiedTopology: true});
+       console.log('DB connected successfully', connect.connection.host, connect.connection.name)
+    }catch(err){
+        console.error(err);
+        process.exit(1);
+    }
+
+    // try{
+    //     const connect = await mongoose.connect(process.env.CONNECTION_STRING,{useNewUrlParser:true, useUnifiedTopology:true, useFindAndModify: false});
+    //     console.log('DB connected successfully', connect.connection.host, connect.connection.name)
+    //  }catch(err){
+    //      console.error(err);
+    //      process.exit(1);
+    //  }
 }
 
 
